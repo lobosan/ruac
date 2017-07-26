bea0d268a51422f7502f615e9a581a459f39375b
lobosan <sp.galindoh@gmail.com>
2017-07-26 11:06:38 -0500

    Server configuration

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,6 @@
-import fallback from 'express-history-api-fallback'
-import express from 'express'
-const app = express()
-const root = `${__dirname}`
+var fallback = require('express-history-api-fallback')
+var express = require('express')
+var app = express()
+var root = `${__dirname}`
 app.use(express.static(root))
 app.use(fallback('index.html', { root }))
