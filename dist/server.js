e0a9147c1ee29474a7b3dc149eaf39016cd0ab46
lobosan <sp.galindoh@gmail.com>
2017-07-26 11:08:36 -0500

    Server configuration

--- dist/server.js
+++ dist/server.js
@@ -4,3 +4,6 @@ var app = express()
 var root = __dirname
 app.use(express.static(root))
 app.use(fallback('index.html', { root: root }))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('Server started ' + port)
