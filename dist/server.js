d8f407f3b044bc206f44718cca88e5e101a8d32b
lobosan <sp.galindoh@gmail.com>
2017-07-26 10:55:12 -0500

    Server history mode and serve-static

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,9 @@
 var express = require('express')
+var serveStatic = require('serve-static')
 var history = require('connect-history-api-fallback')
 var app = express()
-app.use(history())
-app.listen(3000)
-console.log('Server started on port 3000')
+app.use(serveStatic(__dirname))
+app.use(history)
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('Server started ' + port)
