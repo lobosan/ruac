899c71f326f3019f405ce9fd246648ce9de5753d
lobosan <sp.galindoh@gmail.com>
2017-07-26 11:07:30 -0500

    Server configuration

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,6 @@
 var fallback = require('express-history-api-fallback')
 var express = require('express')
 var app = express()
-var root = `${__dirname}`
+var root = __dirname
 app.use(express.static(root))
-app.use(fallback('index.html', { root }))
+app.use(fallback('index.html', { root: root }))
